1aa09c534c15bec3bab2e3041874517911611b2b
mostofa kamal <mostofakamal1990@gmail.com>
2024-01-13 21:45:27 +0600

    Update

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,11 +3,20 @@ import Square from './Square'
 export default function Board() {
 
     const [square, setSquere] = useState(Array(9).fill(null)); 
+    const [isNext, setNext] = useState(true);
 
     function handelClick(index){
+
+        if(square[index] ) return;
         const nextSqures = square.slice();
-        nextSqures[index] = "X";
+        
+        if(isNext){
+            nextSqures[index] = "X";
+        }else{
+            nextSqures[index] = "Y";
+        }
         setSquere(nextSqures);
+        setNext(!isNext);
     }
  
   return (
@@ -27,6 +36,8 @@ export default function Board() {
             <Square value={square[7]} onSquareClicked={()=>handelClick(7)}/>
             <Square value={square[8]} onSquareClicked={()=>handelClick(8)}/>
         </div>
+
+        //fasdfasdfasdfadf
 
     </>
     )
